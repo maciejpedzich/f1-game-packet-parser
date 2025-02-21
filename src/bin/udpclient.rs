d2ded9da511a07fe6df4fc4f798cc6547b62f009
Maciej Pędzich <contact@maciejpedzi.ch>
2025-02-21 18:23:46 +0100

    misc: print UDP client listening message after successful socket bind

--- src/bin/udpclient.rs
+++ src/bin/udpclient.rs
@@ -3,13 +3,16 @@ use std::error::Error;
 use std::net::UdpSocket;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let socket = UdpSocket::bind("127.0.0.1:20777")?;
+    let addr = "127.0.0.1:20777";
+    let socket = UdpSocket::bind(addr)?;
     let mut buf = [0u8; 2048];
 
-    loop {
-        let (msg_size, _) = socket.recv_from(&mut buf)?;
-        let data = &buf[..msg_size];
+    println!("UDP client is listening on {}", addr);
 
-        parse(data)?;
+    loop {
+        let (msg_len, _) = socket.recv_from(&mut buf)?;
+        let msg = &buf[..msg_len];
+
+        parse(msg)?;
     }
 }
